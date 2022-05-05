7e0707a4d2ef516aa3708cc105892ac53ea95bb9
motassem naqawah <motasem.n@genie-soft.com>
2022-05-06 01:41:56 +0300

    added new font fixed q and a screen click

--- app/src/main/java/com/example/chemistryapp/view/main/MainActivity.kt
+++ app/src/main/java/com/example/chemistryapp/view/main/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.chemistryapp.view.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.example.chemistryapp.R
+import com.google.android.material.appbar.MaterialToolbar
 
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -24,6 +28,8 @@ class MainActivity : AppCompatActivity() {
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNav.setOnNavigationItemSelectedListener(navListener)
 
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
         // as soon as the application opens the first
         // fragment should be shown to the user
         // in this case it is algorithm fragment
@@ -33,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         // in this case it is algorithm fragment
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, LessonsFragment()).commit()
+
     }
 
     private val navListener =
@@ -45,10 +52,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_laws -> selectedFragment = LawsFragment()
                 R.id.nav_elements -> selectedFragment = ElementsFragment()
                 R.id.nav_search -> selectedFragment = SearchFragment()
-                R.id.main_q_and_a -> {
-                    val intent = Intent(this,QuestionsActivity::class.java)
-                    startActivity(intent)
-                }
             }
             // It will help to replace the
             // one fragment to other.
@@ -58,4 +61,21 @@ class MainActivity : AppCompatActivity() {
                 .commit()
             true
         }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.main_q_and_a -> {
+                val intent = Intent(this,QuestionsActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
+
+
